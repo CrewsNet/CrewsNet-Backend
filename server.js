fb1cc58492c311d82251f48bf353c707cdd5c020
Prakhar <srivastava11735@gmail.com>
2021-09-01 02:08:24 +0530

    Api Contest Controller

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const DB = process.env.DATABASE;
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true,
   })
   .then(() => console.log("DB connection successful!"));
 
