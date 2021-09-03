4fffe6868bfb829f5e4bc071e93e9618839f99b0
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2021-09-04 03:59:54 +0530

    code-santized errors removed

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
-const express = require("express");
-const morgan = require("morgan");
-const session = require("express-session");
-const dotenv = require("dotenv");
-const MongoStore = require("connect-mongo");
-const passport = require("passport");
-require("./helpers/passportGoogle")(passport);
-require("./helpers/passportGithub")(passport);
+const express = require("express")
+const morgan = require("morgan")
+const session = require("express-session")
+const dotenv = require("dotenv")
+const MongoStore = require("connect-mongo")
+const passport = require("passport")
+require("./helpers/passportGoogle")(passport)
+require("./helpers/passportGithub")(passport)
 
 /* ---------------------------- Function Imports ---------------------------- */
 
-const userRouter = require("./routes/Users/auth/userRoutes");
-const contestRouter = require("./routes/Users/Contests/contest");
-const globalErrorHandler = require("./controllers/Users/errorController");
-const AppError = require("./utils/appError");
+const userRouter = require("./routes/Users/auth/userRoutes")
+const contestRouter = require("./routes/Users/Contests/contest")
+const globalErrorHandler = require("./controllers/Users/errorController")
+const AppError = require("./utils/appError")
 
-const app = express();
-dotenv.config({ path: "./config.env" });
+const app = express()
+dotenv.config({ path: "./config.env" })
 
 /* --------------------------- Express MiddleWares -------------------------- */
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+  app.use(morgan("dev"))
 }
 
 // app.use(express.Router());
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+app.use(express.json())
+app.use(express.static(`${__dirname}/public`))
+
+//CORS POLICY
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
 app.use(
   session({
@@ -35,32 +42,32 @@ app.use(
       mongoUrl: process.env.DATABASE,
     }),
   })
-);
+)
 
 //Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+  req.requestTime = new Date().toISOString()
+  next()
+})
 
 /* ------------------------------ Route Section ----------------------------- */
 
 /* ------------------- Auth Routes ------------------------ */
 
-app.use("/auth", require("./routes/Users/auth/googleAuth"));
-app.use("/auth", require("./routes/Users/auth/githubAuth"));
+app.use("/auth", require("./routes/Users/auth/googleAuth"))
+app.use("/auth", require("./routes/Users/auth/githubAuth"))
 
 /* ---------------------User Routes------------------------ */
-app.use("/users", userRouter);
-app.use("/user", contestRouter);
+app.use("/users", userRouter)
+app.use("/user", contestRouter)
 
 app.all("*", (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-});
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
+})
 
-app.use(globalErrorHandler);
+app.use(globalErrorHandler)
 
-module.exports = app;
+module.exports = app
--- controllers/Users/authController.js
+++ controllers/Users/authController.js
@@ -1,37 +1,35 @@
-const catchAsync = require("./../../utils/catchAsync");
-const jwt = require("jsonwebtoken");
-const AppError = require("./../../utils/appError");
-const sendEmail = require("./../../utils/email");
-const crypto = require("crypto");
+const catchAsync = require("./../../utils/catchAsync")
+const jwt = require("jsonwebtoken")
+const AppError = require("./../../utils/appError")
+const sendEmail = require("./../../utils/email")
+const crypto = require("crypto")
 
 /* ---------------------------- Function Imports ---------------------------- */
 
-const User = require("./../../models/userModel");
+const User = require("./../../models/userModel")
 
 /* ---------------------------- Generating Token ---------------------------- */
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
-  });
-};
+  })
+}
 
 /* ----------------------- Setting Token In the Cookie ---------------------- */
 
 const createSendToken = (user, statusCode, res) => {
-  const token = signToken(user._id);
+  const token = signToken(user._id)
   const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
     httpOnly: true,
-  };
-  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+  }
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true
 
-  res.cookie("jwt", token, cookieOptions);
+  res.cookie("jwt", token, cookieOptions)
 
   // Remove password from output
-  user.password = undefined;
+  user.password = undefined
 
   res.status(statusCode).json({
     status: "success",
@@ -39,84 +37,82 @@ const createSendToken = (user, statusCode, res) => {
     data: {
       user,
     },
-  });
-};
+  })
+}
 
 /* ---------------------------- SignUp Controller --------------------------- */
 
 exports.getSignup = (req, res) => {
-  res.status(200).send("SignUp Page");
-};
+  res.status(200).send("SignUp Page")
+}
 exports.getLogin = (req, res) => {
-  res.status(200).send("Login Page");
-};
+  res.status(200).send("Login Page")
+}
 
-exports.signup = catchAsync(async (req, res, next) => {
+exports.signup = async (req, res, next) => {
   const newUser = new User({
     email: req.body.email,
     password: req.body.password,
     name: req.body.name,
     emailToken: crypto.randomBytes(64).toString("hex"),
-  });
+  })
+  console.log("Req", req.body)
+  console.log(req.body.email, req.body.password, req.body.name)
 
-  await newUser.save();
+  await newUser.save()
 
-  const confirmURL = `${req.protocol}://${req.get(
-    "host"
-  )}/api/users/confirmEmail/${newUser.emailToken}`;
+  const confirmURL = `${req.protocol}://${req.get("host")}/users/confirmEmail?token=${newUser.emailToken}`
 
-  const message = `Confirm Your Email by sending a patch request at: ${confirmURL}.\n`;
+  const message = `Confirm Your Email by sending a patch request at: ${confirmURL}.\n`
 
   try {
     await sendEmail({
       email: newUser.email,
       subject: "Confirm Your Email",
       message,
-    });
+    })
 
     res.status(200).json({
       status: "success",
       message: "Confirmation Mail Sent!",
-    });
+    })
   } catch (err) {
-    newUser.email = undefined;
-    newUser.token = undefined;
-    await newUser.save({ validateBeforeSave: false });
+    console.log("INside Catch Block")
+    newUser.email = undefined
+    newUser.token = undefined
+    await newUser.save({ validateBeforeSave: false })
 
-    return next(
-      new AppError("There was an error sending the email. Try again later!"),
-      500
-    );
+    return next(new AppError("There was an error sending the email. Try again later!"), 500)
   }
-});
+}
 
 /* ---------------------------- Post Login Route ---------------------------- */
 
 exports.login = catchAsync(async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
   if (!email || !password) {
-    return next(new AppError("Please provide Email and Password", 400));
+    return next(new AppError("Please provide Email and Password", 400))
   }
 
-  const user = await User.findOne({ email }).select("+password");
+  const user = await User.findOne({ email }).select("+password")
 
   if (user.confirmSignup === false) {
-    return next(new AppError("Please Confirm Your Email First", 401));
+    return next(new AppError("Please Confirm Your Email First", 401))
   }
 
   if (!user || !(await user.correctPassword(password, user.password))) {
-    return next(new AppError("Incorrect email or password", 401));
+    return next(new AppError("Incorrect email or password", 401))
   }
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, res)
 
   //   const token = signToken(user._id);
 
   res.status(200).json({
     status: "success",
     token,
-  });
-});
+  })
+})
 
 /* ----------------------------- DashBoard Route ---------------------------- */
 
@@ -124,154 +120,145 @@ exports.dashBoard = catchAsync(async (req, res) => {
   res.status(200).json({
     message: "Success",
     data: "Success While Logging in",
-  });
-});
+  })
+})
 
 /* ----------------------- DashBoard Protection Route ----------------------- */
 
 exports.authPass = async (req, res, next) => {
   // 1) Getting token and check of it's there
-  let token;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    token = req.headers.authorization.split(" ")[1];
+  let token
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1]
   } else if (req.cookies.jwt) {
-    token = req.cookies.jwt;
+    token = req.cookies.jwt
   }
 
   if (!token) {
     return res.status(400).json({
       message: "You arent Logged In",
-    });
+    })
   }
 
   // 2) Verification token
-  const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+  const decoded = await jwt.verify(token, process.env.JWT_SECRET)
 
   // 3) Check if user still exists
-  const currentUser = await User.findById(decoded.id);
+  const currentUser = await User.findById(decoded.id)
   if (!currentUser) {
     return res.status(400).json({
       message: "You arent Logged In",
-    });
+    })
   }
 
   // 4) Check if user changed password after the token was issued
 
   // GRANT ACCESS TO PROTECTED ROUTE
-  req.user = currentUser;
-  res.locals.user = currentUser;
-  next();
-};
+  req.user = currentUser
+  res.locals.user = currentUser
+  next()
+}
 
 /* -------------------- Forget PassWord Logic Controller ------------------- */
 
 exports.forgotPassword = catchAsync(async (req, res, next) => {
   // 1) Get user based on POSTed email
-  const user = await User.findOne({ email: req.body.email });
+  const user = await User.findOne({ email: req.body.email })
   if (!user) {
-    return next(new AppError("There is no user with email address.", 404));
+    return next(new AppError("There is no user with email address.", 404))
   }
 
   // 2) Generate the random reset token
-  const resetToken = user.createPasswordResetToken();
-  await user.save({ validateBeforeSave: false });
+  const resetToken = user.createPasswordResetToken()
+  await user.save({ validateBeforeSave: false })
 
   // 3) Send it to user's email
-  const resetURL = `${req.protocol}://${req.get(
-    "host"
-  )}/api/users/resetPassword/${resetToken}`;
+  const resetURL = `${req.protocol}://${req.get("host")}/api/users/resetPassword/${resetToken}`
 
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`
 
   try {
     await sendEmail({
       email: user.email,
       subject: "Your password reset token (valid for 10 min)",
       message,
-    });
+    })
 
     res.status(200).json({
       status: "success",
       message: "Token sent to email!",
-    });
+    })
   } catch (err) {
-    user.passwordResetToken = undefined;
-    user.passwordResetExpires = undefined;
-    await user.save({ validateBeforeSave: false });
+    user.passwordResetToken = undefined
+    user.passwordResetExpires = undefined
+    await user.save({ validateBeforeSave: false })
 
-    return next(
-      new AppError("There was an error sending the email. Try again later!"),
-      500
-    );
+    return next(new AppError("There was an error sending the email. Try again later!"), 500)
   }
-});
+})
 
 exports.resetPassword = catchAsync(async (req, res, next) => {
   // 1) Get user based on the token
-  const hashedToken = crypto
-    .createHash("sha256")
-    .update(req.params.token)
-    .digest("hex");
+  const hashedToken = crypto.createHash("sha256").update(req.params.token).digest("hex")
 
   const user = await User.findOne({
     passwordResetToken: hashedToken,
     passwordResetExpires: { $gt: Date.now() },
-  });
+  })
 
   // 2) If token has not expired, and there is user, set the new password
   if (!user) {
-    return next(new AppError("Token is invalid or has expired", 400));
+    return next(new AppError("Token is invalid or has expired", 400))
   }
-  user.password = req.body.password;
-  user.passwordResetToken = undefined;
-  user.passwordResetExpires = undefined;
-  await user.save();
+  user.password = req.body.password
+  user.passwordResetToken = undefined
+  user.passwordResetExpires = undefined
+  await user.save()
 
   // 3) Update changedPasswordAt property for the user
   // 4) Log the user in, send JWT
-  createSendToken(user, 200, res);
-});
+  createSendToken(user, 200, res)
+})
 
 /* ----------------------- Mail Confirming Controller ----------------------- */
 
 exports.confirmEmail = catchAsync(async (req, res, next) => {
   try {
-    const user = await User.findOne({ emailToken: req.params.token });
+    console.log("Inside Confirm Email Route")
+    const token = req.query.token
+    const user = await User.findOne({ emailToken: token })
 
     if (!user) {
-      return next(new AppError("Token is invalid or has expired", 400));
+      return next(new AppError("Token is invalid or has expired", 400))
     }
 
-    user.emailToken = null;
-    user.confirmSignup = true;
-    await user.save();
-    res.redirect("/login");
+    user.emailToken = null
+    user.confirmSignup = true
+    await user.save()
+    res.redirect("/login")
   } catch (error) {
-    console.log(error);
-    res.redirect("/signup");
+    console.log(error)
+    res.redirect("/signup")
   }
-});
+})
 
 /* ------------------------- Token Conversion Route ------------------------- */
 exports.getToken = async (req, res) => {
-  const token = req.params.token;
-  console.log(token);
+  const token = req.params.token
+  console.log(token)
 
   // 2) Verification token
-  const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+  const decoded = await jwt.verify(token, process.env.JWT_SECRET)
 
   // 3) Check if user still exists
-  const currentUser = await User.findById(decoded.id);
+  const currentUser = await User.findById(decoded.id)
   if (!currentUser) {
     return res.status(400).json({
       message: "You arent Logged In",
-    });
+    })
   }
-  const id = currentUser._id;
+  const id = currentUser._id
   res.status(200).json({
     id: id,
-  });
-};
+  })
+}
