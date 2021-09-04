7e36545292408a2b360403bc40963f35a979acb5
Shivansh Joshi <77457296+sniperzee2@users.noreply.github.com>
2021-09-04 12:20:17 +0530

    google auth modified

--- routes/Users/auth/userRoutes.js
+++ routes/Users/auth/userRoutes.js
@@ -22,18 +22,11 @@ const { ensureAuth, ensureGuest } = require("../../../middleware/googleAuth")
 
 /* -------------------------- Authorization Routes -------------------------- */
 
-<<<<<<< HEAD
-router.post("/signup", signup).get("/signup", getSignup);
-router.post("/login", login).get("/login", getLogin);
-router.get("/dash", authPass, ensureAuth, dashBoard);
-router.patch("/confirmEmail/:id", confirmEmail);
-router.post("/auth/google", googleLogin)
-=======
 router.post("/signup", ensureGuest, signup).get("/signup", getSignup)
 router.post("/login", ensureGuest, login).get("/login", getLogin)
 router.get("/dash", authPass, ensureAuth, dashBoard)
 router.get("/confirmEmail", confirmEmail)
->>>>>>> 4fffe6868bfb829f5e4bc071e93e9618839f99b0
+router.get("/auth/google",googleLogin)
 
 /* -------------------------- Forget PassWord Routes ------------------------- */
 
