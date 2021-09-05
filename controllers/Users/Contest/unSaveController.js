a1a424f9289501db596a0f3b722c36f5e94cd703
Prakhar <srivastava11735@gmail.com>
2021-09-06 03:06:46 +0530

    Unsaving User v.2

--- controllers/Users/Contest/unSaveController.js
+++ controllers/Users/Contest/unSaveController.js
@@ -13,7 +13,7 @@ exports.unSave = (req, res) => {
   } = req.body;
 
   const arr = user.savedContest.filter((obj) => {
-    return obj.url === url && obj.start_time === start_time;
+    return !(obj.url === url && obj.start_time === start_time);
   });
 
   user.savedContest = arr;
