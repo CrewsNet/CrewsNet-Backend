2527238d04fe2f504f1c8b53c6b72033ed78456d
Prakhar <srivastava11735@gmail.com>
2021-09-07 22:33:39 +0530

    Get Info Route

--- routes/Users/Profile/userProfile.js
+++ routes/Users/Profile/userProfile.js
@@ -2,9 +2,12 @@ const express = require("express");
 const { authPass } = require("../../../controllers/Users/authController");
 const {
   profile,
+  getInfo,
 } = require("../../../controllers/Users/Profile/profileController");
 const router = express.Router();
 
 router.get("/profile", authPass, profile);
+
+router.get("/getInfo", authPass, getInfo);
 
 module.exports = router;
